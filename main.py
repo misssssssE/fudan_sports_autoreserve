1303fce0b2298b3f4647d11208907afebe99aa59
misssssssE <56917079+misssssssE@users.noreply.github.com>
2023-10-09 21:43:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         logged_in_session = apis.login(USER_ID, USER_PASSWORD)
         campus_id, sport_id = apis.load_sports_and_campus_id(logged_in_session, SERVICE_CATEGORY, CAMPUS_NAME, SPORT_NAME)
         service_id = apis.get_service_id(logged_in_session, SERVICE_CATEGORY, campus_id, sport_id, SPORT_LOCATION)
-        for i in range(10):
+        for i in range(70):
             apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME)
             
     except Exception as e:
